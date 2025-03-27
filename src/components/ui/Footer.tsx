3d0820bb6d30351c12e1c5e81545da7be4629b84
Michael <michael.venema2010@gmail.com>
2025-03-27 09:06:16 -0700

    Update footer with a hyperlink to venomlabs.xyz

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -6,8 +6,17 @@ export default function Footer() {
   return (
     <footer className=" py-6 px-4 text-sm bg-base-100 text-base-content">
       <div className="max-w-6xl mx-auto flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
-        <p>&copy; {new Date().getFullYear()} Made by <span className="font-bold">VenomLabs</span></p>
-
+      <p>
+        &copy; {new Date().getFullYear()} Made by{" "}
+        <a
+          href="https://venomlabs.xyz"
+          className="font-bold hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          VenomLabs
+        </a>
+      </p>
         <div className="flex items-center space-x-6">
           <Link href="/privacy-policy" className="hover:underline">
             Privacy Policy
